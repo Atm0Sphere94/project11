19186ea64bc64a26e3d983d5151c60003c4d020d
Oleg Malyshev <m_oleg161@mail.ru>
2020-04-17 11:48:14 +0300

    Modified webpack.config and package.json

--- webpack.config.js
+++ webpack.config.js
@@ -4,18 +4,19 @@ const OptimizeCssAssetsPlugin = require('optimize-css-assets-webpack-plugin');
 const isDev = process.env.NODE_ENV === 'development';
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackMd5Hash = require('webpack-md5-hash');
-const MiniCssExtractPlugin = require("mini-css-extract-plugin"); 
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 
 module.exports = {
-  entry: { main: './src/index.js' },
+  entry: {
+    main: './src/index.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].[chunkhash].js',
   },
   module: {
-    rules: [
-      {
+    rules: [{
         test: /\.js$/,
         exclude: /node_modules/,
         use: {
@@ -24,30 +25,31 @@ module.exports = {
       },
       {
         test: /\.css$/i,
-        use: [
-            (isDev ? 'style-loader' : MiniCssExtractPlugin.loader),
-            'css-loader', 
-            'postcss-loader'
-        ]
+        use: {
+          loader: 'css-loader',
+          options: {
+            importLoaders: 2
+          }
+        }
       },
       {
         test: /\.(png|jpg|gif|ico|svg)$/,
         use: [
-             'file-loader?name=./src/images/[name].[ext]', // указали папку, куда складывать изображения
-             {
-                 loader: 'image-webpack-loader',
-                 options: {
-                  bypassOnDebug: true, 
-                  disable: true,
-                }
-             },
-        ],
-        },
-        {
-            test: /\.(eot|ttf|woff|woff2)$/,
-            loader: 'file-loader?name=./vendor/[name].[ext]'
+          'file-loader?name=./src/images/[name].[ext]', // указали папку, куда складывать изображения
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              bypassOnDebug: true,
+              disable: true,
             }
-      
+          },
+        ],
+      },
+      {
+        test: /\.(eot|ttf|woff|woff2)$/,
+        loader: 'file-loader?name=./vendor/[name].[ext]'
+      }
+
 
     ]
   },
@@ -56,16 +58,16 @@ module.exports = {
       filename: 'index.[contenthash].css',
     }),
     new webpack.DefinePlugin({
-        'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
-   }),
-   new OptimizeCssAssetsPlugin({
-    assetNameRegExp: /\.css$/g,
-    cssProcessor: require('cssnano'),
-    cssProcessorPluginOptions: {
-            preset: ['default'],
-    },
-    canPrint: true
-}),
+      'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+    }),
+    new OptimizeCssAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require('cssnano'),
+      cssProcessorPluginOptions: {
+        preset: ['default'],
+      },
+      canPrint: true
+    }),
     new HtmlWebpackPlugin({
       inject: false,
       template: './src/index.html',
